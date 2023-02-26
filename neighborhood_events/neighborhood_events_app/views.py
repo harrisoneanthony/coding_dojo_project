920d77c55e1108cc2e635bca7a049dcf96ac639c
Jonas Schoellkopf <107725945+jschoellkopf@users.noreply.github.com>
2023-02-26 09:17:25 -0800

    Update views.py for validation

--- neighborhood_events/neighborhood_events_app/views.py
+++ neighborhood_events/neighborhood_events_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib import messages
 import datetime
+import bcrypt
 
 todays_date = datetime.datetime.now()
 
@@ -12,6 +13,12 @@ def login(request):
     return render(request, "login.html")
 
 def login_user(request):
+    if request.method == "POST":
+        errors = User.objects.login_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request,value)
+            return redirect('/login')
     logged_in_user = User.objects.get(email=request.POST['email'])
     request.session['id'] = logged_in_user.id
     return redirect('/dashboard')
@@ -49,7 +56,11 @@ def create_user(request):
                 messages.error(request,value)
             return redirect('/register')
         else:
-            User.objects.create(first_name = request.POST["first_name"],last_name = request.POST["last_name"],email = request.POST["email"],dob = request.POST["dob"], password = request.POST["password"])
+            password = request.POST['password']
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+            User.objects.create(first_name = request.POST["first_name"],last_name = request.POST["last_name"],email = request.POST["email"],dob = request.POST["dob"], password = pw_hash)
+            logged_in_user = User.objects.get(email=request.POST['email'])
+            request.session['id'] = logged_in_user.id
     return redirect('/dashboard')
 
 def logout(request):

37515d94fb41ffa41ed298be995bd315285106bc
Jonas Schoellkopf <107725945+jschoellkopf@users.noreply.github.com>
2023-02-26 09:24:45 -0800

    Update models.py validation

--- neighborhood_events/neighborhood_events_app/models.py
+++ neighborhood_events/neighborhood_events_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 import re
+import datetime as dt
+from datetime import datetime
+import bcrypt
+
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+time_now = dt.datetime.now()
 
 class UserManager(models.Manager):
     def user_validator(self, postData):
@@ -9,15 +14,40 @@ class UserManager(models.Manager):
             errors["first_name"] = "First name must be at least 2 characters."
         if len(postData['last_name']) < 2:
             errors["last_name"] = "Last name must be at least 2 characters."
+        all_users = User.objects.all()
+        for user in all_users:
+            if user.email == postData['email']:
+                errors['email'] = "Email address already exists"
         if not EMAIL_REGEX.match(postData['email']):
             errors['email'] = "Invalid email address!"
-        if not re.search("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[#$%&'()*+,-./:;<=>?@[\]^_`{|}~!]).*$", postData['password']):
-            errors['password'] = "Password must be at least 8 characters and contain one number, one upper case character, and one special character."
-        if postData['password'] != postData['confirm_password']:
-            errors['password'] = "Passwords must match", 'confirm_password'
+        # if not re.search("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!?]).*$", postData['password']):
+        #     errors['password'] = "Password must be at least 8 characters and contain one number, one upper case character, and one special character."
+        # if postData['password'] != postData['confirm_password']:
+        #     errors['password'] = "Passwords must match", 'confirm_password'
+        if not postData['dob']:
+            errors['dob'] = "Please select a date of birth"
+        date_of_birth = datetime.strptime(postData['dob'], '%Y-%m-%d')
+        print(date_of_birth.year)
+        if (time_now.year - date_of_birth.year) < 18:
+            errors['dob'] = "Individuals under 18 years old cannot register"
+        if time_now.year - date_of_birth.year == 18:
+            if time_now.month < date_of_birth.month:
+                errors['dob'] = "Individuals under 18 years old cannot register"
+            if time_now.month == date_of_birth.month:
+                if time_now.day < date_of_birth.day:
+                    errors['dob'] = "Individuals under 18 years old cannot register"
         return errors
-    # hello
-#     hello back
+
+    def login_validator(self, postData):
+        errors = {}
+        user = User.objects.filter(email=postData['email'])
+        if user:
+            logged_user = user[0]
+            if not bcrypt.checkpw(postData['password'].encode(),logged_user.password.encode()):
+                errors['password'] = "Email address and password do not match our records"
+        else:
+            errors['email'] = "Email address and password do not match our records"
+        return errors
 
 class EventManager(models.Manager):
     def event_validator(self, postData):
